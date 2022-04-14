78511b6f5096b5e0e5a7db7273234d493a5be93d
Melih Namer <mnamer12@outlook.com>
2022-04-14 21:04:36 +0300

    style:adventurer-idcard & script file

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //Javascript Files of the mighty pirate //
+const idCard = document.querySelector('.adventurer-idcard');
+const idName = idCard.querySelector('.adventurer-idcard__text_name');
+const idClass = idCard.querySelector('.adventurer-idcard__text_class');
+const idPetName = idCard.querySelector('.adventurer-idcard__text_petg');
+
+
