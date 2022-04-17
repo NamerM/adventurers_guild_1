a856d0f6b7f165336ae8a52bf8c45d0bd1cf1380
Melih Namer <mnamer12@outlook.com>
2022-04-17 19:33:11 +0300

    fix: idcard to idCard on filenames

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 //Javascript Files of the mighty pirate //
-const idCard = document.querySelector('.adventurer-idcard');
-const idName = idCard.querySelector('.adventurer-idcard__text_name');
-const idClass = idCard.querySelector('.adventurer-idcard__text_class');
-const idPetName = idCard.querySelector('.adventurer-idcard__text_petg');
+const idCard = document.querySelector('.adventurer-idCard');
+const idName = idCard.querySelector('.adventurer-idCard__text_name');
+const idClass = idCard.querySelector('.adventurer-idCard__text_class');
+const idPetName = idCard.querySelector('.adventurer-idCard__text_petg');
 
 const idForm = documents.forms.idCardForm;
 const idCardPopup = document.querySelector('.popup_type_idCard');
-const idNameButton = document.querySelector('.button_edit_name');
-const idClassButton = document.querySelector('.button_edit_class');
-const idPetButton = document.querySelector('.button_edit_pet');
+const editButton = document.querySelector('.adventurer-idCard ')
+//const idNameButton = document.querySelector('.button_edit_name');
+//const idClassButton = document.querySelector('.button_edit_class');
+//const idPetButton = document.querySelector('.button_edit_pet');
 const closeButton = document.querySelector('.popup__close');
 
-const inputName = document.idCardPopup.querySelector('.adventurer-idcard__text_name');
-const inputClass = document.idCardPopup.querySelector('.adventurer-idcard__text_class');
-const inputPet = document.idCardPopup.querySelector('.adventurer-idcard__text_pet');
+const inputName = document.idCardPopup.querySelector('.popup__input_idCard_name');
+const inputClass = document.idCardPopup.querySelector('.popup__input_idCard_class');
+const inputPetName = document.idCardPopup.querySelector('.popup__input_idCard_pet');
 const popupSelector = 'popup__open';
+
+//functions of Id Card //
+function openPopup (popup) {
+  popup.classList.add(popupSelector);
+  addKeyDownListener(); //
+}
+
+function closePopup (popup) {
+  popup.classList.remove(popupSelector);
+  removeKeyDownListener();
+}
+
+function openProfilePopup () {
+  openPopup(idCardPopup);
+  inputName.value = idName.textContent;
+  inputClass.value = idClass.textContent;
+  inputPetName.value = idPetName.textContent;
+}
+
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  idName.textContent = inputName.value;
+  idClass.textContent = inputClass.value;
+  idPetName.textContent = inputPetName.value;
+  closePopup (idCardPopup);
+}
+
+// Event Handlers for idCard
 
