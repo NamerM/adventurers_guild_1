acb06552810cd1fcefd24b356286392e42807040
Melih Namer <mnamer12@outlook.com>
2022-04-17 18:44:51 +0300

    feat: script file and index.html progress

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ const idName = idCard.querySelector('.adventurer-idcard__text_name');
 const idClass = idCard.querySelector('.adventurer-idcard__text_class');
 const idPetName = idCard.querySelector('.adventurer-idcard__text_petg');
 
+const idForm = documents.forms.idCardForm;
+const idCardPopup = document.querySelector('.popup_type_idCard');
+const idNameButton = document.querySelector('.button_edit_name');
+const idClassButton = document.querySelector('.button_edit_class');
+const idPetButton = document.querySelector('.button_edit_pet');
+const closeButton = document.querySelector('.popup__close');
+
+const inputName = document.idCardPopup.querySelector('.adventurer-idcard__text_name');
+const inputClass = document.idCardPopup.querySelector('.adventurer-idcard__text_class');
+const inputPet = document.idCardPopup.querySelector('.adventurer-idcard__text_pet');
+const popupSelector = 'popup__open';
 
